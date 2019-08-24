0c1876c3e4ab729308029eb1d24a6394a6fade55
NathanSandy <nahtan123@gmail.com>
2019-08-24 16:30:52 +0530

    Third commit

--- src/com/Git_Project/Sample_Git.java
+++ src/com/Git_Project/Sample_Git.java
@@ -5,6 +5,8 @@ public class Sample_Git {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("welcome to Git");
+		System.out.println("Changing the code");
+		System.out.println("for Checkout ");
 		
 
 	}
